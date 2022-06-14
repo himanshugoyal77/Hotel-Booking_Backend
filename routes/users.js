941c6e1b62654ffac3be2ba62f15619ed1e23114
himanshugoyal77 <92782099+himanshugoyal77@users.noreply.github.com>
2022-06-14 21:20:52 +0530

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { deleteUser, getAllUsers, getUser, updateUser } from "../controllers/user.js";
+import User from "../models/User.js";
+import { createError } from "../utils/error.js";
+import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//create
+// router.get("/checkauthentication", verifyToken, (req, res, next)=> {
+//    res.send("hello user you are loggedIn")
+// })
+
+// router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+//   res.send("hello user, you are logged in and you can delete your account")
+// })
+
+// router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+//   res.send("hello admin, you are logged in and you can delete all accounts")
+// })
+
+//UPDATE
+router.put("/:id",verifyUser, updateUser)
+
+//DELETE
+router.delete("/:id",verifyUser, deleteUser)
+
+router.get("/:id",verifyUser, getUser)
+
+//GET ALL
+router.get("/",verifyAdmin, getAllUsers)
+
+
+
+
+export default router;
